4785bac053085b7fbf26210cfa40b3b107c27298
Salini4kamal <98230233+Salini4kamal@users.noreply.github.com>
2022-06-03 15:37:06 +0530

    Add files via upload

---
+++ callback hell/script.js
@@ -0,0 +1,94 @@
+
+var div=document.createElement("div")
+div.setAttribute("id","root")
+div.style="height:100%"
+
+
+document.body.append(div)
+setTimeout(()=>{
+
+div.innerHTML=10
+    
+    setTimeout(()=>{
+
+       div.innerHTML=9
+
+        setTimeout(()=>{
+
+           div.innerHTML=8
+            setTimeout(()=>{
+               div.innerHTML=7
+    
+                setTimeout(()=>{
+                    div.innerHTML=6
+    
+                    setTimeout(()=>{
+                       div.innerHTML=5
+    
+
+                        setTimeout(()=>{
+                            div.innerHTML=4
+    
+                            setTimeout(()=>{
+                                div.innerHTML=3
+    
+                                setTimeout(()=>{
+                                    div.innerHTML=2
+    
+                                    setTimeout(()=>{
+                                       div.innerHTML=1
+    
+                                    setTimeout(()=>{
+                                        div.innerHTML=0
+
+                                   
+setTimeout(()=>{
+    div.setAttribute("id","roo")
+    div.innerHTML="Happy Independence Day"
+},1000)
+
+                                    },1000)
+                                },1000)
+
+                                    },1000)
+
+
+
+
+                                },1000)
+
+
+
+
+                            },1000)
+
+
+
+
+                        },1000)
+
+
+
+                    },1000)
+
+
+
+
+                },1000)
+
+
+
+
+            },1000)
+
+
+
+
+        },1000)
+
+
+
+
+ },1000)
+
+  
